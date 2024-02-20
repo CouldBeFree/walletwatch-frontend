a1ac5fd0ef775d88c5d1e48589f8a0819d5f1de4
couldBeFree <zoriaalexandr@gmail.com>
2024-02-20 19:46:01 +0200

    Updated row width

--- src/layouts/auth/AuthLayout.vue
+++ src/layouts/auth/AuthLayout.vue
@@ -3,7 +3,7 @@
     <v-main>
       <v-container class="fill-height d-flex align-center justify-center">
         <v-row>
-          <v-col sm="4" offset-sm="4" xl="2" offset-xl="5">
+          <v-col cols="3" sm="4" offset-sm="4">
             <router-view />
           </v-col>
         </v-row>
