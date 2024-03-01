41b1176b757aa0244c28b5a99779724f9526890d
couldBeFree <zoriaalexandr@gmail.com>
2024-03-01 15:57:37 +0200

    Created expense composable

---
+++ src/composable/Expenses/useUserExpenses.js
@@ -0,0 +1,51 @@
+import {onMounted, ref} from "vue";
+import {expensesStore} from "@/store/expenses";
+import {storeToRefs} from "pinia";
+import proxy from "@/utils/proxy";
+import {FIRE_SNACK} from "@/constants";
+import getErrorMessage from "@/utils/getErrorMessage";
+
+export default function useUserExpenses() {
+  const store = expensesStore();
+  const { getUsersExpenses, getAllExpenses } = storeToRefs(store);
+
+  const loading = ref(false);
+
+  onMounted(async () => {
+    loading.value = true;
+    await store.getUsersExpensesFromApi();
+    await store.getAllExpensesFromApi();
+    loading.value = false;
+  });
+
+  const onDeleteExpense = async (id) => {
+    loading.value = true;
+    try {
+      await store.removeCategoryExpense(id);
+      await store.getUsersExpensesFromApi();
+      proxy.publish(FIRE_SNACK, { type: 'green', text: 'Success' });
+    } catch (e) {
+      const errorMsg = getErrorMessage(e);
+      proxy.publish(FIRE_SNACK, { type: 'red', text: errorMsg });
+    } finally {
+      loading.value = false;
+    }
+  }
+
+  const onCreateExpense = async (ids) => {
+    const promises = ids.map((id) => store.createExpense(id));
+    loading.value = true;
+    try {
+      await Promise.all(promises);
+      await store.getUsersExpensesFromApi();
+      proxy.publish(FIRE_SNACK, { type: 'green', text: 'Success' });
+    } catch (e) {
+      const errorMsg = getErrorMessage(e);
+      proxy.publish(FIRE_SNACK, { type: 'red', text: errorMsg });
+    } finally {
+      loading.value = false;
+    }
+  }
+
+  return { getUsersExpenses, getAllExpenses, onCreateExpense, onDeleteExpense, loading }
+}
--- src/views/Expenses.vue
+++ src/views/Expenses.vue
@@ -4,8 +4,8 @@
     <v-col xs="12" sm="12" md="5" lg="3">
       <UserExpenses
         :data="getAllExpenses"
-        :userData="expensesFromStore"
-        :loading="loading"
+        :userData="getUsersExpenses"
+        :loading="expenseLoading"
         @delete="onDeleteExpense"
         @create="onCreateExpense"
       />
@@ -23,53 +23,10 @@
 import ExpenseChartStatistic from "@/components/ExpenseChartStatistic.vue";
 import UserExpenses from "@/components/UserExpenses.vue";
 import TransactionExpenses from "@/components/TransactionExpenses.vue";
-import { storeToRefs } from "pinia";
-import { expensesStore } from "@/store/expenses";
-import {onMounted, ref} from "vue";
-import getErrorMessage from "@/utils/getErrorMessage";
-import proxy from "@/utils/proxy";
-import { FIRE_SNACK } from "@/constants";
+import useUserExpenses from "@/composable/Expenses/useUserExpenses";
 
-const store = expensesStore();
-const { getUsersExpenses: expensesFromStore, getAllExpenses } = storeToRefs(store);
+const { getAllExpenses, getUsersExpenses, loading: expenseLoading, onDeleteExpense, onCreateExpense } = useUserExpenses();
 
-const loading = ref(false);
-
-onMounted(async () => {
-  loading.value = true;
-  await store.getUsersExpensesFromApi();
-  await store.getAllExpensesFromApi();
-  loading.value = false;
-});
-
-const onDeleteExpense = async (id) => {
-  loading.value = true;
-  try {
-    await store.removeCategoryExpense(id);
-    await store.getUsersExpensesFromApi();
-    proxy.publish(FIRE_SNACK, { type: 'green', text: 'Success' });
-  } catch (e) {
-    const errorMsg = getErrorMessage(e);
-    proxy.publish(FIRE_SNACK, { type: 'red', text: errorMsg });
-  } finally {
-    loading.value = false;
-  }
-}
-
-const onCreateExpense = async (ids) => {
-  const promises = ids.map((id) => store.createExpense(id));
-  loading.value = true;
-  try {
-    await Promise.all(promises);
-    await store.getUsersExpensesFromApi();
-    proxy.publish(FIRE_SNACK, { type: 'green', text: 'Success' });
-  } catch (e) {
-    const errorMsg = getErrorMessage(e);
-    proxy.publish(FIRE_SNACK, { type: 'red', text: errorMsg });
-  } finally {
-    loading.value = false;
-  }
-}
 </script>
 
 <style scoped></style>
